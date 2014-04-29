587f8599e59a001587d14e5d00ae543ff82b657a
mic <m@micz.it>
2014-04-29 22:57:28 +0200

    readded mziq-overlay.js for miczImapQuota definition

---
+++ chrome/content/mziq-overlay.js
@@ -0,0 +1,8 @@
+"use strict";
+var miczImapQuota = {
+
+  //Default Thunderbird options
+  defaultThresholdShow: 75,
+  defaultThresholdWarning: 80,
+  defaultThresholdCritical: 95,
+};
--- chrome/content/mziq-prefobserver.js
+++ chrome/content/mziq-prefobserver.js
@@ -1,5 +1,4 @@
 "use strict";
-var miczImapQuota={};
 /**
  * @constructor
  *
