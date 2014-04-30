b2d05b2161e9063f6369e325fb2a456efd4463fb
Mic <m@micz.it>
2014-04-30 20:58:54 +0200

    fixed a problem in setting the show pref using the checkbox

--- chrome/content/mziq-settings.js
+++ chrome/content/mziq-settings.js
@@ -7,11 +7,13 @@ var miczImapQuotaPref = {
 
   update_show_value: function(){
     let prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
-    prefs = prefs.getBranch("extensions.SavedSearchThemAll.");
+    prefs = prefs.getBranch(miczImapQuota.baseBranch);
     if(document.getElementById("ImapQuota.Show_checkbox").checked){
       document.getElementById("ImapQuota.ThresholdShow_textfield").value=0;
+      prefs.setIntPref("show",0);
     }else{
       document.getElementById("ImapQuota.ThresholdShow_textfield").value=miczImapQuota.defaultThresholdShow;
+      prefs.setIntPref("show",miczImapQuota.defaultThresholdShow);
     }
   },
 
