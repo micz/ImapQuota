cdf011dab062faf00e67481bc699e1d165ba82bb
Mic <m@micz.it>
2014-05-17 10:46:19 +0200

    added set default button. to be localized. fixes #1

--- chrome/content/mziq-settings.js
+++ chrome/content/mziq-settings.js
@@ -16,5 +16,21 @@ var miczImapQuotaPref = {
       prefs.setIntPref("show",miczImapQuota.defaultThresholdShow);
     }
   },
+  
+  set_default_values: function(){
+    var promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"].getService(Components.interfaces.nsIPromptService);
+    let t_msg="Are you sure?";
+    let p_msg_c="Do you want to set alle the values to the default ones?";
+    if(!promptService.confirm(null,t_msg,p_msg_c))return;
+    
+    let prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
+    prefs = prefs.getBranch(miczImapQuota.baseBranch);
+    document.getElementById("ImapQuota.ThresholdShow_textfield").value=miczImapQuota.defaultThresholdShow;
+    prefs.setIntPref("show",miczImapQuota.defaultThresholdShow);
+    document.getElementById("ImapQuota.ThresholdWarning_textfield").value=miczImapQuota.defaultThresholdWarning;
+    prefs.setIntPref("warning",miczImapQuota.defaultThresholdWarning);
+    document.getElementById("ImapQuota.ThresholdCritical_textfield").value=miczImapQuota.defaultThresholdCritical;
+    prefs.setIntPref("critical",miczImapQuota.defaultThresholdCritical);    
+  },
 
 };
